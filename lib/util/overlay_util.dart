7e8bc4c19a22563d1b14fc3b89af4521cf0aca19
JianYe <monian@momoko.cc>
2021-07-28 22:55:07 +0800

    底部弹出输入框针对安卓优化

--- lib/util/overlay_util.dart
+++ lib/util/overlay_util.dart
@@ -7,35 +7,37 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_moko/ui/fast_widget.dart';
 
 class OverlayUtil {
-  static Future<bool> showTipDialog(BuildContext context, {
+  static Future<bool> showTipDialog(
+    BuildContext context, {
     String? title,
     String? tip,
     String? cancelText,
     String? confirmText,
   }) async {
     return await showCupertinoDialog<bool>(
-      context: context,
-      builder: (context) {
-        return CupertinoAlertDialog(
-          title: Text(title ?? "提示"),
-          content: Text(tip ?? "确认执行此操作吗?"),
-          actions: <Widget>[
-            TextButton(
-              child: Text(cancelText ?? "取消"),
-              onPressed: () => Navigator.of(context).pop(false),
-            ),
-            TextButton(
-              child: Text(confirmText ?? "确认"),
-              onPressed: () => Navigator.of(context).pop(true),
-            ),
-          ],
-        );
-      },
-    ) ??
+          context: context,
+          builder: (context) {
+            return CupertinoAlertDialog(
+              title: Text(title ?? "提示"),
+              content: Text(tip ?? "确认执行此操作吗?"),
+              actions: <Widget>[
+                TextButton(
+                  child: Text(cancelText ?? "取消"),
+                  onPressed: () => Navigator.of(context).pop(false),
+                ),
+                TextButton(
+                  child: Text(confirmText ?? "确认"),
+                  onPressed: () => Navigator.of(context).pop(true),
+                ),
+              ],
+            );
+          },
+        ) ??
         false;
   }
 
-  static Future<String?> showBottomSheetTextField(BuildContext context, {
+  static Future<String?> showBottomSheetTextField(
+    BuildContext context, {
     String? value,
     List<TextInputFormatter>? inputFormatters,
     String? cancelText,
@@ -50,17 +52,16 @@ class OverlayUtil {
       isScrollControlled: true,
       backgroundColor: Colors.white,
       barrierColor: Colors.transparent,
-      builder: (context) =>
-          BottomSheetTextField(
-            value: value,
-            inputFormatters: inputFormatters,
-            cancelText: cancelText,
-            confirmText: confirmText,
-            cancelColor: cancelColor,
-            cancelTextColor: cancelTextColor,
-            confirmColor: confirmColor,
-            confirmTextColor: confirmTextColor,
-          ),
+      builder: (context) => BottomSheetTextField(
+        value: value,
+        inputFormatters: inputFormatters,
+        cancelText: cancelText,
+        confirmText: confirmText,
+        cancelColor: cancelColor,
+        cancelTextColor: cancelTextColor,
+        confirmColor: confirmColor,
+        confirmTextColor: confirmTextColor,
+      ),
     );
   }
 }
@@ -109,48 +110,53 @@ class _BottomSheetTextFieldState extends State<BottomSheetTextField> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200,
-      padding: EdgeInsets.all(25),
-      child: Column(
-        children: [
-          if (widget.title != null) Text(widget.title!),
-          TextField(
-            controller: _textEditingController,
-            inputFormatters: widget.inputFormatters,
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              fillColor: Colors.black12,
-              filled: true,
-            ),
-            style: TextStyle(
-              color: Colors.black,
-            ),
-          ),
-          SizedBox(
-            height: 25,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              FastWidget.textButton(
-                widget.cancelText ?? "取消",
-                backgroundColor: widget.cancelColor ?? Colors.black,
-                textColor: widget.cancelTextColor ?? Colors.white,
-                onPressed: () => Navigator.of(context).pop(null),
+    return SingleChildScrollView(
+      child: Container(
+        padding: EdgeInsets.only(
+          top: 25,
+          left: 25,
+          right: 25,
+          bottom: MediaQuery.of(context).viewInsets.bottom + 25,
+        ),
+        child: Column(
+          children: [
+            if (widget.title != null) Text(widget.title!),
+            TextField(
+              controller: _textEditingController,
+              inputFormatters: widget.inputFormatters,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                fillColor: Colors.black12,
+                filled: true,
               ),
-              FastWidget.textButton(
-                widget.confirmText ?? "确认",
-                backgroundColor: widget.confirmColor ?? Colors.black,
-                textColor: widget.confirmTextColor ?? Colors.white,
-                onPressed: () =>
-                    Navigator.of(context).pop(
-                      _textEditingController.text,
-                    ),
+              style: TextStyle(
+                color: Colors.black,
               ),
-            ],
-          )
-        ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                FastWidget.textButton(
+                  widget.cancelText ?? "取消",
+                  backgroundColor: widget.cancelColor ?? Colors.black,
+                  textColor: widget.cancelTextColor ?? Colors.white,
+                  onPressed: () => Navigator.of(context).pop(null),
+                ),
+                FastWidget.textButton(
+                  widget.confirmText ?? "确认",
+                  backgroundColor: widget.confirmColor ?? Colors.black,
+                  textColor: widget.confirmTextColor ?? Colors.white,
+                  onPressed: () => Navigator.of(context).pop(
+                    _textEditingController.text,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
